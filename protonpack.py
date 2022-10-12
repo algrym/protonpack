5dbfc175b73eeba091cef92e5caaa0d421e7eee4
A. J. Wright <ajw@toadking.org>
2022-10-11 20:00:21 -0400

    Changes to drive Neopixel Stick like power cell

--- protonpack.py
+++ protonpack.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import time
 
@@ -7,8 +8,24 @@ import digitalio
 import microcontroller
 import neopixel
 
-# We know there's at least one
+# We know there's at least one, we'll discover if more
 number_cpus = 1
+
+# Update this to match the number of NeoPixel LEDs connected to your board.
+neopixel_stick_num_pixels = 20
+
+# Which pin is the stick DIN connected to?
+neopixel_stick_pin = board.GP0
+
+# How fast should the neopixel stick cycle?
+neopixel_stick_speed = 40
+
+# Color constants
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+OFF = (0, 0, 0)
 
 
 def print_startup():
@@ -17,6 +34,29 @@ def print_startup():
     print(f" - python v{sys.version}")
     print(f" - cpu count: {number_cpus}")
     print(f" - neopixel v{neopixel.__version__}")
+
+
+def neopixel_run():
+    pixels = neopixel.NeoPixel(neopixel_stick_pin, neopixel_stick_num_pixels)
+    pixels.brightness = 0.008  # 0.008 is the dimmest I can make them
+    sleep_duration = 1 / neopixel_stick_speed
+
+    led = digitalio.DigitalInOut(board.LED)
+    led.direction = digitalio.Direction.OUTPUT
+
+    # Make sure all the pixels are working
+    for c in (OFF, RED, GREEN, BLUE, WHITE):
+        pixels.fill(c)
+        time.sleep(30 * sleep_duration)
+
+    while True:
+        max_pixel = random.randrange(0, len(pixels))
+        # for i in range(len(pixels)):
+        for i in range(max_pixel):
+            pixels[i] = BLUE
+            led.value = i % 2
+            time.sleep(sleep_duration)
+        pixels.fill(OFF)
 
 
 def update_cpu_count() -> int:
@@ -28,32 +68,13 @@ def update_cpu_count() -> int:
     return count
 
 
-def heartbeat_led():
-    led = digitalio.DigitalInOut(board.LED)
-    led.direction = digitalio.Direction.OUTPUT
-    while True:
-        led.value = True
-        time.sleep(0.1)
-        led.value = False
-        time.sleep(0.1)
-        led.value = True
-        time.sleep(0.1)
-        led.value = False
-        time.sleep(0.5)
-
-
 def main() -> int:
     update_cpu_count()
     print_startup()
-    heartbeat_led()
-    return 0;
+    neopixel_run()
+    return 0
 
 
 # main
 if __name__ == '__main__':
     sys.exit(main())
-
-# pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, auto_write=False)
-# pixels[0] = (10, 0, 0)
-# pixels[9] = (0, 10, 0)
-# pixels.show()
