096b0271d1cec8e60160f9acc648160da0c9dfcb
A. J. Wright <ajw@toadking.org>
2022-10-21 19:43:02 -0400

    Changes necessary to control both the stick and the ring

--- protonpack.py
+++ protonpack.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import os
 import random
 import sys
@@ -5,23 +6,22 @@ import time
 
 import board
 import digitalio
-import microcontroller
 import neopixel
 
-# We know there's at least one, we'll discover if more
-number_cpus = 1
+# Software version
+protonpack_version: str = '0.4'
 
 # Update this to match the number of NeoPixel LEDs connected to your boards
-neopixel_stick_num_pixels = 20
-neopixel_ring_num_pixels = 20
+neopixel_stick_num_pixels: int = 20
+neopixel_ring_num_pixels: int = 20
 
 # Which pins are the stick and ring connected to?
 # (These should be different, but you do you.)
 neopixel_stick_pin = board.GP1
 neopixel_ring_pin = board.GP0
 
-neopixel_stick_brightness = 0.008  # 0.008 is the dimmest I can make them
-neopixel_ring_brightness = 0.02  # 0.008 is the dimmest I can make them
+neopixel_stick_brightness: float = 0.008  # 0.008 is the dimmest I can make them
+neopixel_ring_brightness: float = 0.02  # 0.008 is the dimmest I can make them
 
 # How fast should the neopixel cycle?
 neopixel_stick_speed = 40
@@ -36,10 +36,9 @@ OFF = (0, 0, 0)
 
 
 def print_startup():
-    print("-=< protonpack v0.3 - https://github.com/algrym/protonpack/ >=-")
+    print(f"-=< protonpack v{protonpack_version} - https://github.com/algrym/protonpack/ >=-")
     print(f" - uname: {os.uname()}")
     print(f" - python v{sys.version}")
-    print(f" - cpu count: {number_cpus}")
     print(f" - neopixel v{neopixel.__version__}")
 
 
@@ -50,48 +49,58 @@ def neopixel_run():
     stick_pixels.brightness = neopixel_stick_brightness
     ring_pixels.brightness = neopixel_ring_brightness
 
-    stick_sleep_duration = 1 / neopixel_stick_speed
-    ring_sleep_duration = 1 / neopixel_ring_speed
-
-    led = digitalio.DigitalInOut(board.LED)
-    led.direction = digitalio.Direction.OUTPUT
+    # Set up the LED on-board the pico to indicate the clock
+    onboard_led = digitalio.DigitalInOut(board.LED)
+    onboard_led.direction = digitalio.Direction.OUTPUT
 
     # Make sure all the pixels are working
-    for c in (OFF, RED, GREEN, BLUE, WHITE):
+    for c in (OFF, RED, GREEN, BLUE, WHITE, OFF):
         stick_pixels.fill(c)
-        time.sleep(30 * stick_sleep_duration)
+        ring_pixels.fill(c)
+        time.sleep(1)
 
+    # Main driver loop
+    clock = ring_cursor = 0
+    stick_cursor = stick_max = 0
     while True:
-        previous = len(ring_pixels) - 1
-        for i in range(len(ring_pixels)):
-            ring_pixels[i] = RED
-            ring_pixels[previous] = RED
-            led.value = i % 2
-            time.sleep(ring_sleep_duration)
-            ring_pixels[previous] = OFF
-            previous = i
+        # if the clock is a multiple of the speed,
+        #   check the status of the pixel at the cursor.
+        #      Turn it on if it's off.
+        #      Turn it off if it's on, and increment the cursor.
+        if clock % neopixel_ring_speed == 0:
+            if ring_pixels[ring_cursor] == OFF:
+                ring_pixels[ring_cursor] = RED
+            else:
+                ring_pixels[ring_cursor] = OFF
+                ring_cursor += 1
+        if ring_cursor >= len(ring_pixels):
+            ring_cursor = 0
 
-    # this loop works just for the blue power meter
-    # while True:
-    #     max_pixel = random.randrange(0, len(stick_pixels))
-    #     for i in range(max_pixel):
-    #         stick_pixels[i] = BLUE
-    #         led.value = i % 2
-    #         time.sleep(stick_sleep_duration)
-    #     stick_pixels.fill(OFF)
+        # increment the power cell
+        if clock % neopixel_stick_speed == 0:
+            if stick_cursor >= stick_max:
+                stick_max = random.randrange(0, len(stick_pixels))
+                stick_cursor = 0
+                stick_pixels.fill(OFF)
+            stick_pixels[stick_cursor] = BLUE
+            stick_cursor += 1
+        if stick_cursor >= len(stick_pixels):
+            stick_pixels = 0
 
+        # reset counters periodically to avoid overflow
+        if (ring_cursor == 0) and (stick_cursor == 0):
+            print("Resetting clock and LEDs")
+            stick_pixels.fill(OFF)
+            clock = 0
 
-def update_cpu_count() -> int:
-    global number_cpus
-    count = 0
-    for cpu in microcontroller.cpus:
-        count += 1
-    number_cpus = count
-    return count
+        # increment the LED and clock
+        onboard_led.value = clock % 2
+        clock += 1
+
+        time.sleep(0.1)
 
 
 def main() -> int:
-    update_cpu_count()
     print_startup()
     neopixel_run()
     return 0
