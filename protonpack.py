6c441353dbc2741be138ab477effd7f97c5729dd
A. J. Wright <ajw@toadking.org>
2022-11-20 14:45:57 -0500

    Reduced function depth for performance

--- protonpack.py
+++ protonpack.py
@@ -5,11 +5,10 @@ import sys
 import time
 
 import board
-import digitalio
 import neopixel
 
 # Software version
-protonpack_version: str = '0.4'
+protonpack_version: str = '0.5'
 
 # Update this to match the number of NeoPixel LEDs connected to your boards
 neopixel_stick_num_pixels: int = 20
@@ -29,91 +28,72 @@ neopixel_stick_speed = 6
 neopixel_ring_speed = 3
 onboard_led_speed = 20
 
+# Print startup info
+print(f"-=< protonpack v{protonpack_version} - https://github.com/algrym/protonpack/ >=-")
+print(f" - uname: {os.uname()}")
+print(f" - python v{sys.version}")
+print(f" - neopixel v{neopixel.__version__}")
+
 # Color constants
 RED = (255, 0, 0)
+ORANGE = (255, 165, 0)
+YELLOW = (255, 255, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+PURPLE = (128, 0, 128)
 WHITE = (255, 255, 255)
 OFF = (0, 0, 0)
 
+# initialize neopixels
+stick_pixels = neopixel.NeoPixel(neopixel_stick_pin, neopixel_stick_num_pixels)
+ring_pixels = neopixel.NeoPixel(neopixel_ring_pin, neopixel_ring_num_pixels)
 
-def print_startup():
-    print(f"-=< protonpack v{protonpack_version} - https://github.com/algrym/protonpack/ >=-")
-    print(f" - uname: {os.uname()}")
-    print(f" - python v{sys.version}")
-    print(f" - neopixel v{neopixel.__version__}")
+# set brightness
+stick_pixels.brightness = neopixel_stick_brightness
+ring_pixels.brightness = neopixel_ring_brightness
 
+# Make sure all the pixels are working
+for c in (OFF, RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, WHITE, OFF):
+    stick_pixels.fill(c)
+    ring_pixels.fill(c)
+    time.sleep(0.1)
 
-def neopixel_run():
-    stick_pixels = neopixel.NeoPixel(neopixel_stick_pin, neopixel_stick_num_pixels)
-    ring_pixels = neopixel.NeoPixel(neopixel_ring_pin, neopixel_ring_num_pixels)
+# set up main driver loop
+clock = ring_cursor = 0
+ring_previous = len(ring_pixels) - 1
+stick_cursor = stick_max = 0
 
-    stick_pixels.brightness = neopixel_stick_brightness
-    ring_pixels.brightness = neopixel_ring_brightness
+# main driver loop
+while True:
+    # if the clock is a multiple of the speed,
+    #   check the status of the pixel at the cursor.
+    #      Turn it on if it's off.
+    #      Turn it off if it's on, and increment the cursor.
+    if clock % neopixel_ring_speed == 0:
+        if ring_pixels[ring_cursor] == OFF:
+            ring_pixels[ring_cursor] = RED
+            ring_pixels[ring_previous] = RED
+        else:
+            ring_pixels[ring_cursor] = OFF
+            ring_pixels[ring_previous] = OFF
+            ring_previous = ring_cursor
+            ring_cursor += 1
+    if ring_cursor >= len(ring_pixels):
+        ring_cursor = 0
+    if ring_previous >= len(ring_pixels):
+        ring_previous = 0
+        clock = 0
 
-    # Set up the LED on-board the pico to indicate the clock
-    onboard_led = digitalio.DigitalInOut(board.LED)
-    onboard_led.direction = digitalio.Direction.OUTPUT
-
-    # Make sure all the pixels are working
-    for c in (OFF, RED, GREEN, BLUE, WHITE, OFF):
-        stick_pixels.fill(c)
-        ring_pixels.fill(c)
-        time.sleep(1)
-
-    # Main driver loop
-    clock = ring_cursor = 0
-    ring_previous = len(ring_pixels) - 1
-    stick_cursor = stick_max = 0
-    while True:
-        # if the clock is a multiple of the speed,
-        #   check the status of the pixel at the cursor.
-        #      Turn it on if it's off.
-        #      Turn it off if it's on, and increment the cursor.
-        if clock % neopixel_ring_speed == 0:
-            if ring_pixels[ring_cursor] == OFF:
-                ring_pixels[ring_cursor] = RED
-                ring_pixels[ring_previous] = RED
-            else:
-                ring_pixels[ring_cursor] = OFF
-                ring_pixels[ring_previous] = OFF
-                ring_previous = ring_cursor
-                ring_cursor += 1
-        if ring_cursor >= len(ring_pixels):
-            ring_cursor = 0
-        if ring_previous >= len(ring_pixels):
-            ring_previous = 0
-
-        # increment the power cell
-        if clock % neopixel_stick_speed == 0:
-            if stick_cursor >= stick_max:
-                stick_max = random.randrange(0, len(stick_pixels))
-                stick_cursor = 0
-                stick_pixels.fill(OFF)
-            stick_pixels[stick_cursor] = BLUE
-            stick_cursor += 1
-        if stick_cursor >= len(stick_pixels):
-            stick_pixels = 0
-
-        # reset counters periodically to avoid overflow
-        if (ring_cursor == 0) and (stick_cursor == 0):
-            print("Resetting clock and LEDs")
+    # increment the power cell
+    if clock % neopixel_stick_speed == 0:
+        if stick_cursor >= stick_max:
+            stick_max = random.randrange(0, len(stick_pixels))
+            stick_cursor = 0
             stick_pixels.fill(OFF)
-            clock = 0
+            ring_pixels[ring_cursor] = WHITE  # flash the ring element to white
+        stick_pixels[stick_cursor] = BLUE
+        stick_cursor += 1
+    if stick_cursor >= len(stick_pixels):
+        stick_pixels = 0
 
-        # increment the LED and clock
-        onboard_led.value = clock % onboard_led_speed
-        clock += 1
-
-        time.sleep(sleep_duration)
-
-
-def main() -> int:
-    print_startup()
-    neopixel_run()
-    return 0
-
-
-# main
-if __name__ == '__main__':
-    sys.exit(main())
+    clock += 1
